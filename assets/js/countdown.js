2a57d733f86deac40442c122347da951f75ff29f
Rostacia <rostacia_lewis@hotmail.com>
2022-12-15 15:21:27 -0500

    Change event start time for countdown and make agenda updates

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 16, 2022 10:00:00").getTime();
+var countDownDate = new Date("Dec 16, 2022 9:10:00").getTime();
 				
 // Update the count down every 1 second
 var x = setInterval(function() {
